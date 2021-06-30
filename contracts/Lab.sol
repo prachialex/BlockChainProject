20e4c112e7d501d6ed6192fac954d6dd459be1c7
Prachi Alex <46198116+prachialex@users.noreply.github.com>
2021-06-30 11:48:26 +0530

    few changes

--- contracts/Lab.sol
+++ contracts/Lab.sol
@@ -1,13 +1,25 @@
 pragma solidity ^0.8.0;
 
 contract Lab{
-    
+      
     struct Labs{
-        string name;
-        uint256 no;
-        address _labAddress;
-        string desc;
-      //  State s;
+      uint256 labId;
+      bool approved;
+    }
+    string result;
+    mapping (address=>Labs) public labs;
+
+    function requestApproval(address _lab) private view
+    {
+      require(labs[_lab].approved==false,"The lab is already approved");
+
+
+    }
+
+    function generateReports(string memory _pName, uint256 _uId, string memory _result) private
+    {
+
+
     }
 
 
